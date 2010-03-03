2aeb16cfc5ec045e650d97e0b7d616a3510575b2
Kevin McFadden <kevin@conceptsahead.com>
2010-03-03 14:17:06 -0700

    merge custom code from deploy.rb

--- lib/vlad-helpers.rb
+++ lib/vlad-helpers.rb
@@ -1,4 +1,21 @@
 namespace :vlad do
+  def require_site(site)
+    if (site.nil? || site.size == 0)
+      puts "Usage: rake vlad:custom:setup[<domain-key>]\n  e.g. rake vlad:custom:setup[www]"
+      puts "       rake vlad:deploy[<domain-key>]\n  e.g. rake vlad:deploy[www]"
+      exit(-1)
+    end
+    set :domain, @domain_map[site]
+  end
+
+  def custom_release(branch)
+    if (branch.nil? || branch.size == 0)
+      set :branch, nil
+    else
+      set :release_name, release_name + "_" + branch
+      set :branch, branch
+    end
+  end
 
   remote_task :set_perms do
     begin
@@ -12,8 +29,13 @@ namespace :vlad do
 
   task :bundle do
     if (application.size > 0)
+      git_cmd = branch ? "git clone -b #{branch} #{git_connection}:#{application}.git /tmp/#{release_name}" :
+                         "git clone #{git_connection}:#{application}.git /tmp/#{release_name}"
+
+
+
       sh [  "rm -rf /tmp/#{release_name}",
-            "git clone #{git_connection}:#{application}.git /tmp/#{release_name}",
+            git_cmd,
             "cd /tmp && tar --exclude=.git -czf #{release_name}.tgz #{release_name}"
         ].join(" && ")
     else
@@ -40,4 +62,41 @@ namespace :vlad do
     end
   end
 
-end
+  namespace :custom do
+
+    desc "Custom rollback to specify deploy target."
+    task :rollback, :site do |t, args|
+      require_site(args[:site])
+      Rake::Task['vlad:custom:rollback_without_restart'].invoke
+      Rake::Task['vlad:custom:symlink_config_dir'].invoke
+    end
+
+    remote_task :rollback_without_restart do
+      if releases.length < 2 then
+        abort "could not rollback the code because there is no prior release"
+      else
+        run "rm -f #{current_path}; ln -s #{previous_release} #{current_path} && rm -rf #{current_release}"
+      end
+    end
+
+    desc "Custom setup requiring deploy target -- use this instead of vlad:setup"
+    task :setup, :site do |t, args|
+      require_site(args[:site])
+
+      Rake::Task['vlad:setup'].invoke
+      #Rake::Task['vlad:custom:share_config_dir'].invoke
+      Rake::Task['vlad:set_perms'].invoke
+    end
+
+    remote_task :share_config_dir do
+      config_dir = File.join(shared_path, "config")
+      run "mkdir -p #{config_dir}"
+    end
+
+    remote_task :symlink_config_dir do
+      run "ln -s #{shared_path}/config #{latest_release}/config"
+    end
+
+  end # End custom namespace
+
+end # End vlad namespace
